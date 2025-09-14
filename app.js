b7cc702758b97736321a1e47a37aaa433bc742e2
unknown <hm3636690@gmail.com>
2025-09-14 14:04:15 +0500

    Created JS file and completed the task

---
+++ app.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button");
+let body = document.querySelector("body");
+
+
+btn.addEventListener("click", function(){
+    body.style.backgroundColor = "black"
+    body.style.color = "white"
+})
